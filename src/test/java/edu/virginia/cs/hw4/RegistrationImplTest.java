c8dc12b674a137c2b2c3d64d045199264a1a65d8
Samira Samadi <ss5ug@virginia.edu>
2023-03-28 13:49:37 -0400

    isEnrollmentFull tests

--- src/test/java/edu/virginia/cs/hw4/RegistrationImplTest.java
+++ src/test/java/edu/virginia/cs/hw4/RegistrationImplTest.java
@@ -1,14 +1,40 @@
 package edu.virginia.cs.hw4;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class RegistrationImplTest {
+    private Student mockStudent;
+    private Course mockCourse;
+    private RegistrationImpl registration;
+    private CourseCatalog courseCatalog;
+
+    @BeforeEach
+    public void setUp(){
+        mockCourse = mock(Course.class);
+        mockStudent = mock(Student.class);
+        courseCatalog = new CourseCatalog();
+        registration = new RegistrationImpl();
+        registration.setCourseCatalog(courseCatalog);
+    }
 
     @Test
-    void isEnrollmentFull() {
+    void isEnrollmentFullWhenCapIsReached() {
+        when(mockCourse.getCurrentEnrollmentSize()).thenReturn(25);
+        when(mockCourse.getEnrollmentCap()).thenReturn(25);
+        assertTrue(registration.isEnrollmentFull(mockCourse));
     }
+
+    @Test
+    void isEnrollmentFullWhenCapIsNotReached() {
+        when(mockCourse.getCurrentEnrollmentSize()).thenReturn(20);
+        when(mockCourse.getEnrollmentCap()).thenReturn(25);
+        assertFalse(registration.isEnrollmentFull(mockCourse));
+    }
+
 
     @Test
     void isWaitListFull() {
