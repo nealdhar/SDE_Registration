ae15674d6dbbabaac2ff084c2c665198b7741876
Samira Samadi <ss5ug@virginia.edu>
2023-03-27 20:59:33 -0400

    PART B TESTS: refactored

--- src/test/java/edu/virginia/cs/hw4/StudentTest.java
+++ src/test/java/edu/virginia/cs/hw4/StudentTest.java
@@ -48,15 +48,15 @@ public class StudentTest {
 
     @Test
     public void testGetCourseGradeWithoutTakingClass() {
-        when(testStudent.hasStudentTakenCourse(mockCourse)).thenReturn(false);
+        when(mockCourseHistory.containsKey(mockCourse)).thenReturn(false);
         assertThrows(IllegalArgumentException.class, () -> testStudent.getCourseGrade(mockCourse));
     }
 
     @Test
     public void testGetCourseGradeWithTakingClass() {
-        when(testStudent.hasStudentTakenCourse(mockCourse)).thenReturn(true);
+        when(mockCourseHistory.containsKey(mockCourse)).thenReturn(true);
         when(mockCourseHistory.get(mockCourse)).thenReturn(Grade.A_MINUS);
-        testStudent.getCourseGrade(mockCourse);
+        assertEquals(Grade.A_MINUS, testStudent.getCourseGrade(mockCourse));
         verify(mockCourseHistory).get(mockCourse);
     }
 
@@ -73,26 +73,26 @@ public class StudentTest {
         when(mockCourseHistory.get(mockCourse)).thenReturn(Grade.C_MINUS);
         assertFalse(testStudent.meetsPrerequisite(prerequisite));
         //verify(mockCourseHistory).containsKey(mockCourse);
-        verify(mockCourseHistory).get(mockCourse);
+        //verify(mockCourseHistory).get(mockCourse);
     }
 
     @Test
     public void testMeetsPrerequisiteWithAllConditionsMet() {
         when(mockCourseHistory.containsKey(mockCourse)).thenReturn(true);
         when(mockCourseHistory.get(mockCourse)).thenReturn(Grade.C_PLUS);
-        testStudent.meetsPrerequisite(prerequisite);
+        assertTrue(testStudent.meetsPrerequisite(prerequisite));
         //verify(mockCourseHistory).containsKey(mockCourse);
-        verify(mockCourseHistory).get(mockCourse);
+        //verify(mockCourseHistory).get(mockCourse);
     }
 
     @Test
     public void testGetGPANoCoursesTaken() {
         when(mockCourseHistory.isEmpty()).thenReturn(true);
         assertThrows(IllegalStateException.class, () -> testStudent.getGPA());
-
     }
+
     @Test
-    public void testGetGPAOneCreditCourse() {
+    public void testGetGPAOneCourse() {
         mockCourseHistory = new HashMap<>();
         testTranscript = new Transcript(testStudent, mockCourseHistory);
         testStudent = new Student(1, "Neal", "nd2pvz@virginia.edu", testTranscript);
@@ -101,9 +101,7 @@ public class StudentTest {
         testStudent.addCourseGrade(mockCourse, Grade.B_PLUS);
         double expectedGPA = Grade.B_PLUS.gpa;
 
-        when(mockCourse.getCreditHours()).thenReturn(3);
         assertEquals(expectedGPA, testStudent.getGPA(), 0.0001);
-
         verify(mockCourse, times(1)).getCreditHours();
     }
 
